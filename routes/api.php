7c33c5be96ab68588753a1eceadb7422e795ce3e
Victor Osipov <victorosipovprogrammer@gmail.com>
2024-07-11 10:23:02 +0300

    add jwt token

---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Post\IndexController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::prefix('auth')->middleware('api')->group(function ($router) {
+
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+
+});
+
+Route::middleware('jwt.auth')->group(function () {
+    Route::get('/posts', IndexController::class);
+
+});
